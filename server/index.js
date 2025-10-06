31f5c46a06ad4fa82a2ae4012a732acc598faf32
AryanR <aryan.resham@gmail.com>
2025-10-06 21:28:29 +0530

    feat: backend setup

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (_req, res) => {
+  res.send('Welcome to the Ride Mates API!');
+});
+
+
+
+
+// RUN
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
